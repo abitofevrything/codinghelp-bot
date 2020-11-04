9aae1d24e970ebdef32ec303ae3dfafc3a347fc5
Erin Skidds <dudethatserin@gmail.com>
2020-11-04 16:03:15 -0500

    Adding & Updating Commands
    Added some commands and updated the elaborate

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,6 @@
-const { prefix, config } = require('/root/bots/ch-bot/config.json');
+const { prefix, config } = require('../config.json');
 const Discord = require("discord.js");
+var client = require("../index.js").client;
 
 const helpEmbed = new Discord.MessageEmbed()
     .setColor('#0099ff')
@@ -11,16 +12,44 @@ const helpEmbed = new Discord.MessageEmbed()
         { name: 'Utilities', value: '```css\nhelp\nping\n```', inline: true },
     );
 
-    module.exports = {
-        name: 'help', // name the command something
-        description: 'Displays all information regarding commands', // Describe your command; shows this with the help command
-        aliases: ['h', 'halp', 'commands'], // Include if you have other names you want to use for this command as well.
-        usage: '[command name]',
-        inHelp: 'yes',
-        execute(message, args) {
-    
-            message.channel.send(helpEmbed);
-    
-        },
-        
-    };
+	module.exports = {
+		name: 'help', // name the command something
+		description: 'Displays all information regarding commands', // Describe your command; shows this with the help command
+		aliases: ['h', 'halp', 'commands'], // Include if you have other names you want to use for this command as well.
+		usage: '[command name]',
+		inHelp: 'yes',
+		execute(message, args) {
+		if(args.length > 0) {
+			var cmd = client.commands.get(args[0]) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(args[0]));
+			if(!cmd) return message.channel.send("That command could not be found!");
+			if(!cmd.inHelp) return message.channel.send("No help for that command could be found!");
+			else{
+				const emb = new Discord.MessageEmbed().setColor(16773617).setTitle(`Help for \`${cmd.name}\``);
+				if(cmd.descripton){
+					emb.setDescription(cmd.descripton);
+				}else{
+					emb.setDescription("No description could be found");
+				}
+				if(cmd.usage){
+					emb.addField("Usage", cmd.usage, true);
+				}
+				if(cmd.aliases){
+					emb.addField("Aliases", cmd.aliases.join(", "), true);
+				}
+				if(cmd.aliases && command.aliases.length > 0){
+					var aliases = "```\n";
+					cmd.aliases.forEach(alias => {
+						useage += alias;
+					});
+					aliases += "```";
+					emb.addField("Aliases", aliases, false);
+				}
+				message.channel.send(emb);
+			}
+		}else{
+			message.channel.send(helpEmbed);
+		}
+	
+		},
+		
+	};
