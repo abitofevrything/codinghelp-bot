14c6afee8f11636f0fad49f4f065c19efbd150a3
Erin Skidds <dudethatserin@gmail.com>
2020-11-10 08:39:36 -0500

    Adding new commands
    Adding hire and docs.

---
+++ commands/hire.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'hire', // name the command something
+    description: 'Refers people to the hire-programmers channel to hire someone.', // Describe your command; shows this with the help command
+    aliases: ['requests'], // Include if you have other names you want to use for this command as well.
+    execute(message, args) {
+  
+    message.channel.send(`🏓 ${Math.round(message.client.ws.ping)} ms`);
+    },
+    
+  };
