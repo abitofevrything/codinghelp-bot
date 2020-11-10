f19ff10a71f54262b76fc95b57e6e49b6897455a
Erin Skidds <dudethatserin@gmail.com>
2020-11-10 09:42:52 -0500

    Update just-ask.js

--- commands/just-ask.js
+++ commands/just-ask.js
@@ -3,6 +3,7 @@ module.exports = {
 	description: 'Tells users to just ask their question instead of asking if someone is here or if someone can help them.', // Describe your command; shows this with the help command
 	aliases: ['ja', 'ask'], // Include if you have other names you want to use for this command as well.
 	usage: '++[command]',
+	inHelp: 'yes',
     execute(message, args) {
 
         message.channel.send("Please just ask your question. Rule 4 prohibits you from asking if someone is available or if someone can help you. Please include any code that is necessary. If you have any questions please refer to <#383032186317832202>. Thank you!")
