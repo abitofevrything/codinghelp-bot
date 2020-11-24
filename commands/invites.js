ea8a5ac9dfd732e498efac576ff439dddd403e80
Erin Skidds <dudethatserin@gmail.com>
2020-11-24 18:50:57 -0500

    Updates
    Updates

---
+++ commands/invites.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: 'invite',
+    description: 'Pushes an invite link.',
+    aliases: ['link'],
+    usage: '.invite',
+    inHelp: 'yes',
+    execute(message, args) {
+
+        message.channel.createInvite()
+        .then(invite => message.reply(`Here is your invite code~ https://discord.gg/${invite.code}`))
+        .catch(console.error);  
+
+  },
+  
+};
