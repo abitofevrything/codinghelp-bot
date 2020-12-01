b7e327b917838bedc1441fe6198e5a6a6db343e7
Erin Skidds <dudethatserin@gmail.com>
2020-11-30 20:17:16 -0500

    Updates
    Updates

--- contestData.js
+++ contestData.js
@@ -2,13 +2,23 @@ const fs = require('fs');
 
 module.exports = {
     getData : function() {
-        return JSON.parse(fs.readFileSync('contestData.txt'));
+        try {
+            let data = JSON.parse(fs.readFileSync('contestData.txt').toString());
+            return data;
+        } catch (e) {
+            return {
+                challenges : [],
+                participants : {}
+            }
+        }
     },
 
     setData : function(data) {
         fs.writeFile('contestData.txt', JSON.stringify(data), (err) => {
-            console.err('Unable to save data (' + err + '). Dumping data to console for recovery');
-            console.log(JSON.stringify(data));
+            if (err) {
+                console.errpr('Unable to save data (' + err + '). Dumping data to console for recovery');
+                console.log(JSON.stringify(data));
+            }
         })
     }
 
