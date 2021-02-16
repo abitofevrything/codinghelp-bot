400e563837a1e13617a879a0c130738c73029404
Erin Skidds <dudethatserin@gmail.com>
2021-02-16 10:07:32 -0500

    Update denied-sugg.js

---
+++ commands/denied-sugg.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js')
+
+module.exports = {
+    name: 'deniedsugg',
+    aliases: ['denys', 'nosugg', 'deniedsuggestion', 'deniedsuggestions', 'denysugg'],
+    inHelp: 'yes',
+    description: 'Denies a specific suggestion. **Note:** This can only be ran by moderators.',
+    usage: '++deniedsugg messageID [reason]',
+    execute(message, args, guild, client) {
+
+    }
+});
