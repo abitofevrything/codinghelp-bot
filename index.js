224a7c3fabfec897f2d0c51c206c31fb4dbfec31
Erin Skidds <dudethatserin@gmail.com>
2021-02-16 14:26:15 -0500

    Updates
    Updates

--- index.js
+++ index.js
@@ -1,72 +1,138 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.commands = new Discord.Collection();
 const {config, token, prefix} = require('./config.json');
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+/* Files */
+const firstMessage = require('./first-message');
+const command = require('./command');
+const privateMessage = require('./private-message');
+const roleClaim = require('./role-claim');
 
-for (const file of commandFiles) {
-	console.log(file.slice(0,-3));
-	const command = require(`./commands/${file.slice(0,-3)}`)
-	client.commands.set(command.name, command);
-}
-
+/* Temporary Commands */
 client.on('ready', () => {
   console.log(`${client.user.tag} is logged in and ready!`);
 
-  client.user.setPresence({
-    status: "dnd", 
-    activity: {
-        name: `++help`,  
-        type: "LISTENING" 
-    }
+  command(client, ['ping', 'test'], (message) => {
+    message.channel.send('Pong!')
   });
+
+  command(client, 'servers', (message) => {
+    client.guilds.cache.forEach((guild) => {
+      message.channel.send(
+        `${guild.name} has a total of ${guild.memberCount} members`
+      )
+    });
 });
 
-client.on('error', function (err) {
-  console.log('Global error handler called:\n');
-  if(err) {console.log(err);}
-});
-
-client.on('message', message => { // Looking for a message
-
-  /* ----------------------------------------------------------
-  NECESSARY PARTS OF THE FILE
-  -------------------------------------------------------------- */
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
-  
-  const args = message.content.slice(prefix.length).trim().split(/ +/);
-  const commandName = args.shift().toLowerCase();
-  
-  const command = client.commands.get(commandName)
-    || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-  
-  if (!command) return;
-  
-  if (!message.guild) return;
-  
-  if (command.guildOnly && message.channel.type !== 'text') {
-    return message.reply('I can\'t execute that command inside DMs!');
-  }
-  
-  if (command.args && !args.length) {
-    let reply = `You didn't provide any arguments, ${message.author}!`;
-  
-    if (command.usage) {
-      reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+command(client, ['cc', 'clearchannel'], (message) => {
+    if (message.member.hasPermission('ADMINISTRATOR')) {
+      message.channel.messages.fetch().then((results) => {
+        message.channel.bulkDelete(results)
+      });
     }
-  
-    return message.channel.send(reply);
+});
+
+command(client, 'ban', (message) => {
+  const { member, mentions } = message;
+
+  const tag = `<@${member.id}>`;
+
+  if (
+    member.hasPermission('ADMINISTRATOR') ||
+    member.hasPermission('BAN_MEMBERS')
+  ) {
+    const target = mentions.users.first()
+    if (target) {
+      const targetMember = message.guild.members.cache.get(target.id);
+      targetMember.ban()
+      message.channel.send(`${tag} That user has been`);
+    } else {
+      message.channel.send(`${tag} Please specify someone to ban.`);
+    }
+  } else {
+    message.channel.send(
+      `${tag} You do not have permission to use this command.`
+    );
   }
-  
-  try {
-    command.execute(message, args);
-  } catch (error) {
-    console.error(error);
-    message.reply('there was an error trying to execute that command!');
+});
+
+command(client, 'kick', (message) => {
+  const { member, mentions } = message;
+
+  const tag = `<@${member.id}>`;
+
+  if (
+    member.hasPermission('ADMINISTRATOR') ||
+    member.hasPermission('KICK_MEMBERS')
+  ) {
+    const target = mentions.users.first()
+    if (target) {
+      const targetMember = message.guild.members.cache.get(target.id);
+      targetMember.kick()
+      message.channel.send(`${tag} That user has kicked`);
+    } else {
+      message.channel.send(`${tag} Please specify someone to kick.`);
+    }
+  } else {
+    message.channel.send(
+      `${tag} You do not have permission to use this command.`
+    );
   }
-  
-  }); // end of looking
+});
+
+command(client, 'help', (message) => {
+  message.channel.send(`
+These are my supported commands:
+**!help** - Displays the help menu
+**!add <num1> <num2>** - Adds two numbers
+**!sub <num1> <num2>** - Subtracts two numbers
+`)
+});
+
+command(client, 'status', (message) => {
+    const content = message.content.replace('++status ', '')
+    // "!status hello world" -> "hello world"
+
+    client.user.setPresence({
+      activity: {
+        name: `"${prefix}help" for help`,
+      },
+    });
+});
+
+roleClaim(client);
+
+command(client, 'serverinfo', (message) => {
+  const { guild } = message
+
+  const { name, region, memberCount, owner, afkTimeout } = guild
+  const icon = guild.iconURL()
+
+  const embed = new Discord.MessageEmbed()
+    .setTitle(`Server info for "${name}"`)
+    .setThumbnail(icon)
+    .addFields(
+      {
+        name: 'Region',
+        value: region,
+      },
+      {
+        name: 'Members',
+        value: memberCount,
+      },
+      {
+        name: 'Owner',
+        value: owner.user.tag,
+      },
+      {
+        name: 'AFK Timeout',
+        value: afkTimeout / 60,
+      }
+    )
+
+  message.channel.send(embed);
+});
+});
 
 client.login(token);
