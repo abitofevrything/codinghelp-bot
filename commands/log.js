e04c4683a96741c11080028c77f3774bd7f7d7ed
Erin Skidds <dudethatserin@gmail.com>
2021-02-22 20:10:07 -0500

    New
    New

---
+++ commands/log.js
@@ -0,0 +1,40 @@
+Promise {
+    PromiseConnection {
+      _events: [Object: null prototype] {
+        newListener: [Function],
+        removeListener: [Function]
+      },
+      _eventsCount: 2,
+      _maxListeners: undefined,
+      connection: Connection {
+        _events: [Object: null prototype],
+        _eventsCount: 1,
+        _maxListeners: undefined,
+        config: [ConnectionConfig],
+        stream: [Socket],
+        _internalId: 0,
+        _commands: [Denque],
+        _command: undefined,
+        _paused: false,
+        _paused_packets: [Denque],
+        _statements: [LRUCache],
+        serverCapabilityFlags: 3254779903,
+        authorized: true,
+        sequenceId: 3,
+        compressedSequenceId: 0,
+        threadId: 30714,
+        _handshakePacket: [Handshake],
+        _fatalError: null,
+        _protocolError: null,
+        _outOfOrderPackets: [],
+        clientEncoding: 'utf8',
+        packetParser: [PacketParser],
+        serverEncoding: 'latin1',
+        connectTimeout: null,
+        connectionId: 30714,
+        [Symbol(kCapture)]: false
+      },
+      Promise: [Function: Promise],
+      [Symbol(kCapture)]: false
+    }
+  }
