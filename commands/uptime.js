2e76afbc02074ed2bab50b8da23a1f4852f5e5e6
Erin Skidds <dudethatserin@gmail.com>
2021-03-28 15:19:20 -0400

    Working uptime command!
    Got it working, finally!

--- commands/uptime.js
+++ commands/uptime.js
@@ -4,13 +4,12 @@ module.exports = {
     name: 'uptime',
     description: 'Tells you the uptime for the bot.',
     aliases: [],
-    usage: 's!uptime',
-    inHelp: 'yes',
-    execute(message, client) { 
-        let days = Math.floor(client.uptime / 86400000);
-        let hours = Math.floor(client.uptime / 3600000) % 24;
-        let minutes = Math.floor(client.uptime / 60000) % 60;
-        let seconds = Math.floor(client.uptime / 1000) % 60;
+    usage: '++uptime',
+    execute(message, args) { 
+        let days = Math.floor(message.client.uptime / 86400000);
+        let hours = Math.floor(message.client.uptime / 3600000) % 24;
+        let minutes = Math.floor(message.client.uptime / 60000) % 60;
+        let seconds = Math.floor(message.client.uptime / 1000) % 60;
         
         message.channel.send(`__Uptime:__\n${days}d ${hours}h ${minutes}m ${seconds}s`);
     }
