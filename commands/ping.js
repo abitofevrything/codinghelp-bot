a8b1495287c0e67380a2742bd84b0feaaf565862
Erin Skidds <dudethatserin@gmail.com>
2021-03-28 15:25:07 -0400

    Combined ping and uptime commands
    Combined the commands so you can run one to see if the bot is online and how long it has been online.

--- commands/ping.js
+++ commands/ping.js
@@ -1,12 +1,15 @@
 module.exports = {
   name: 'ping',
   description: 'Makes sure the bot can hear commands.',
-  aliases: [],
+  aliases: ['uptime', 'beep', 'boop', 'alive'],
   usage: 's!ping',
   inHelp: 'yes',
   execute(message, args) { 
-    if(message.member.roles.cache.has('780941276602302523') || message.member.roles.cache.has('718253309101867008')) 
-    return message.channel.send(`🏓 Pong!`)
-    else {message.reply(':x: You do not have permission to use this command!');}
+    let days = Math.floor(message.client.uptime / 86400000);
+    let hours = Math.floor(message.client.uptime / 3600000) % 24;
+    let minutes = Math.floor(message.client.uptime / 60000) % 60;
+    let seconds = Math.floor(message.client.uptime / 1000) % 60;
+
+   message.channel.send(`🏓 Pong! I have been awake for ${days}d ${hours}h ${minutes}m ${seconds}s`);
   }
 };
