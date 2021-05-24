505ef264f3e02fef24f56eb60c67c5f11b726080
Erin Skidds <dudethatserin@outlook.com>
2021-05-24 09:24:02 -0400

    DELETED
    DELETED

--- mongodb.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-module.exports = async () => {
-  await mongoose.connect("mongodb://adminErin:Tessie5567@localhost:27017/test", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  return mongoose
-}
