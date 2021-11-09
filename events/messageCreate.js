1bdb7b99f5488e9ad599c90f178fad4e664d8f6a
Erin Skidds <dudethatserin@outlook.com>
2021-11-08 20:11:13 -0500

    Updated messageCreate.js and removed the deleted-roles as well as made it so that the mods did not get pinged when a user ran a command that did not exist.

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -56,7 +56,7 @@ module.exports = {
 
                 if (value == modRoles.length) {
                     message.react('❌');
-                    message.reply({ content: `This is a command only moderators can use. You do not have the required permissions. Moderators have the <@&${modRoles[0]}> role or <@&${modRoles[1]}> role or <@&${modRoles[2]}> role  or <@&${modRoles[3]}> role. Please run \`${config.prefix}report [issue]\` if you are seeing this in error.` });
+                    message.reply({ content: `This is a command only moderators can use. You do not have the required permissions. Moderators have the \`<@&${modRoles[0]}>\` role or \`<@&${modRoles[1]}>\` roles. Please run \`${config.prefix}report [issue]\` if you are seeing this in error.` });
                     return;
                 }
             }
@@ -73,45 +73,6 @@ module.exports = {
                     message.reply({ content: `This is a command only challenge moderators can use. You do not have the required permissions. Challenge moderators have the <@&${chllMod[1]}> role. Please run \`${config.prefix}report [issue]\` if you are seeing this in error.` });
                     return;
                 }
-            }
-        }
-
-        if (command.userPerms.length > 0) {
-            let memberChannelPermissions = message.channel.permissionsFor(message.member);
-            memberChannelPermissions = new Discord.Permissions(memberChannelPermissions.bitfield);
-            if (!memberChannelPermissions.has(command.userPerms)) {
-                let missingPermissions = command.userPerms.filter(perm => memberChannelPermissions.has(perm) === false).join(', ');
-                let currentPermissions = command.botPerms.filter(perm => memberChannelPermissions.has(perm) === true).join(', ');
-                console.error(`I can\'t execute this command, ${message.author.tag} with ID of ${message.author.id} is missing permissions these perms: ${missingPermissions}.\nThey currently have these perms: ${currentPermissions}.`);
-                const userPermsEmbed = new Discord.MessageEmbed()
-                    .setColor('RED')
-                    .setTitle('Oh no! An _error_ has appeared!')
-                    .setDescription(`**Contact Bot Owner:** <@${me.id}>`)
-                    .addFields({
-                        name: '**Error Name:**',
-                        value: `\`MISSING PERMISSIONS\``
-                    }, {
-                        name: '**Error Message:**',
-                        value: `${message.author.tag} is missing these permissions for this command:\n\`\`\`${missingPermissions}\`\`\``
-                    }, {
-                        name: '**Message That Triggered Error:**',
-                        value: `\`\`\`Message Content: ${message.content}\nBy: ${message.author.tag} - ${message.author.id}\nLocated in the Channel: ${message.channel.name}\nChannel ID: ${message.channel.id}\nMessage ID: ${message.id}\`\`\``
-                    }, {
-                        name: `**${message.author.username}\'s Current Roles:**`,
-                        value: `\`\`\`${currentPermissions}\`\`\``
-                    }, {
-                        name: '**Ways to Fix:**',
-                        value: 'In order to resolve this error, a moderator needs to go into either this channel or their server\'s role settings and give you the permissions that she is stating you are missing above. If they aren\'t sure how to do that, you can give them [this link](https://support.discord.com/hc/en-us/articles/206029707-How-do-I-set-up-permissions-).'
-                    }, {
-                        name: '**Ways to Report:**',
-                        value: `If you or the mods feel like you are still receiving this message in error. Please take screenshots of the permissions that are set for **${message.author.tag}** and use the options below to report the error to the developer.\nRun the \`${config.prefix}report\` command, [Join My Support Server](https://discord.gg/tT3VEW8AYF), [Fill out this form](https://codinghelp.site/contact-us/) (Erin owns CodingHelp so that form goes directly to her), Message her on Discord, or Email her at me@dudethatserin.site\n\nPlease include all of the information in this embed (message) as well as any additional information you can think to provide. Screenshots are also VERY helpful. Thank you!`
-                    })
-                    .setTimestamp()
-                    .setFooter(`Thanks for using ${client.user.tag}! I'm sorry you encountered this error!`, `${client.user.displayAvatarURL()}`);
-                client.users.cache.get(message.member.id).send(userPermsEmbed);
-                message.react('❌');
-                message.reply({ content: `You do not have the required permissions. If your DMs are open, I have sent you a DM on the matter. Just in case you don\'t have your DMs open, the permissions you need are:\n\`\`\`${missingPermissions}\`\`\` and your current permissions are:\n\`\`\`${currentPermissions}\`\`\`If you feel you are receiving this message in error, open your DMs and run the command again to get ways to report this to the developer.` })
-                return;
             }
         }
 
